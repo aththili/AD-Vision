01dc3da08f6ae0c3c2e0f37cf8b1de604458f7de
aththili <152675838+aththili@users.noreply.github.com>
2024-07-17 19:48:46 +0530

    Update Banner.script.js

--- Banner.script.js
+++ Banner.script.js
@@ -1,7 +1,6 @@
 
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.js"></script>
+
 <script>
            $(document).ready(function(){
                //scroll slides on swipe for touch enabled devices
